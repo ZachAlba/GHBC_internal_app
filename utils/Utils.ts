5bc8f051928b6f75504a49f619aadabc9754685c
Zach <zachalba@uri.edu>
2025-04-25 21:03:12 -0400

    fixed months calculation

--- utils/Utils.ts
+++ utils/Utils.ts
@@ -1,6 +1,6 @@
 export const getCurrentSeason = (): string => {
     const now = new Date();
     const year = now.getFullYear();
-    const month = now.getMonth() + 1; // JS months are 0-indexed
-    return (month >= 5 && month <= 9) ? `S${year}` : `W${year}`;
+    const month = now.getMonth() + 1; 
+    return (month >= 6 && month <= 9) ? `S${year}` : `W${year}`;
   };
